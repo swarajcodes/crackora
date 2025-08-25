cc6d647f844b3b1196acf75e8f925528458819dc
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2025-08-25 19:42:43 +0530

    ARCJET and t3oss for env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,31 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { env } from "./data/env/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)","/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/"]);
+
+const aj = arcjet({
+  key: env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
 
 export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 });
+  }
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
