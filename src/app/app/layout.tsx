19e61d8908c057b3011c3985bf607a2aa22d27f4
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2025-08-30 23:33:56 +0530

    feat: add user avatar component and integrate into Navbar

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -2,6 +2,7 @@ import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
 import { redirect } from "next/navigation";
 
 import { ReactNode } from "react";
+import { Navbar } from "./_Navbar";
 
 export default async function AppLayout({ children }: { children: ReactNode }) {
   const { userId, user } = await getCurrentUser({ allData: true });
@@ -9,5 +10,10 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
   if (userId == null) return redirect("/");
   if (user == null) return redirect("/onboarding");
 
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar user={user} />
+      {children}
+    </>
+  );
 }
